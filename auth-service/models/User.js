fd99ccc864baedd2fc5bdfad038653794e4d66d9
TrinhDinhQuy <162324912+dinhquycoder2k5@users.noreply.github.com>
2025-11-03 13:44:26 +0700

    Update User.js

--- auth-service/models/User.js
+++ auth-service/models/User.js
@@ -1,1 +1,20 @@
-đă
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true
+},
+  password: {
+    type: String,
+    require: true
+},
+  role: {
+    type: String,
+    default: 'user'
+}
+},
+  timestamps: true
+});
+const User = mongoose.model('user', userSchema);
+module.exports = User;
