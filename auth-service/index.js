b99c2b7b3d138c3eb3e04ee391b882452de01ba5
TrinhDinhQuy <162324912+dinhquycoder2k5@users.noreply.github.com>
2025-11-03 14:31:59 +0700

    Update index.js

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,1 +1,31 @@
+/* 
+Starting server
+Reading env file to take key
+Use this key to connect to MongoDB
+*/
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//Read env file
+dotenv.config();
+//Create a instance of server
+const app = express();
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB is connected!');
+  })
+  .catch((err) => {
+    console.error('Connection is failed!', err);
+});
+//Middleware to read JSON
+app.use(express.json());
+//Redirect to "route/auth.js"
+app.use('/auth', require('./route/auth.js');
+//Starting server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Auth Service đang chạy trên cổng ${PORT}`);
+});
 
